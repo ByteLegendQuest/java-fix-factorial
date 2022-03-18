fb4e5e0bb14a7e89b982464a187a6a905f3a21d6
hinkleung <394869042@qq.com>
2022-03-18 17:10:30 +0000

    Update at 2022-03-18T17:10:30.810808Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,9 +13,6 @@ public class Challenge {
 
     public static int factorial(int number) {
         if (number == 0) {
-            return 0;
-        }
-        if (number == 1) {
             return 1;
         }
         return factorial(number - 1) * number;
