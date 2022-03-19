2bddf576c625bfbe9b0703bfc24d2629916a8926
wangjie-fourth <wangjie_fourth@163.com>
2022-03-19 09:09:26 +0000

    Update at 2022-03-19T09:09:26.824261Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
 
     public static int factorial(int number) {
         if (number == 0) {
-            return 0;
+            return 1;
         }
         if (number == 1) {
             return 1;
