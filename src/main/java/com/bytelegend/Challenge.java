533dd924abcdb458ba2a72bb94fbc487c43d801f
KFig21 <Kfig21@gmail.com>
2022-04-27 06:09:27 +0000

    Update at 2022-04-27T06:09:27.035361Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,8 +12,7 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if (number == 0) return 0;
-        if (number == 1) return 1;
+        if (number <= 1) return 1;
         return factorial(number - 1) * number;
     }
 }
