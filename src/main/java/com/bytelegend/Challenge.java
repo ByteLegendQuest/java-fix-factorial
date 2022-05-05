71621d20ee8f4208bdd6818f332087daac635629
Yeely0162 <Yeely01621@163.com>
2022-05-05 15:38:33 +0000

    Update at 2022-05-05T15:38:33.467708Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,8 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
+        if (number==0) return 1;
+        else
         return factorial(number - 1) * number;
     }
 }
