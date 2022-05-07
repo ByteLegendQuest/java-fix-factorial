2a16c57cd2d0191b9ea054b9501120329d9f3b5e
kangming06 <kangming06@hotmail.com>
2022-05-07 07:24:09 +0000

    Update at 2022-05-07T07:24:09.575187Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,7 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if number != 0{
+        if (number != 0) {
             return factorial(number - 1) * number;
         }
         else {
