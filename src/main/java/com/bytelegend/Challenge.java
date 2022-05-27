4febce25ceee48eae9bef901eece1393bbcaf80a
chinahzh <2987140244@qq.com>
2022-05-27 06:40:23 +0000

    Update at 2022-05-27T06:40:23.092253Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,11 +12,9 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if(x<=1)
+        if(number<=1)
             return 1;
         else
             return factorial(number - 1) * number;
-        
-        
     }
 }
