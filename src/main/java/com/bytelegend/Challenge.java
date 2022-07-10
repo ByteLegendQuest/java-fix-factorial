73aca8a596763dc753e4013bf7b2cc8a2061093c
Jungmin-Moon <jmmoon96@gmail.com>
2022-07-10 21:26:39 +0000

    Update at 2022-07-10T21:26:39.060870Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,7 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if (n == 0) {
+        if (number == 0) {
             return 1;
         }
         return factorial(number - 1) * number;
