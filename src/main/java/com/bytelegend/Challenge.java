cbf864288897e056997255ab2210a8a4ca3ccdef
DescentDD <1740811552@qq.com>
2022-07-15 07:02:08 +0000

    Update at 2022-07-15T07:02:08.484088Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,9 +12,9 @@ public class Challenge {
     
 
     public static int factorial(int number) {
-         { if {number == 0}
-        return 1; }
-
+            if ( number == 0 ){
+            return 1; 
+        }
         return factorial(number - 1) * number;
     }
 }
