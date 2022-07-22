90c76adb9409246ea074473c316c9df06b071201
Crystalyyds <2031291160@qq.com>
2022-07-22 07:51:55 +0000

    Update at 2022-07-22T07:51:55.537873Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -16,7 +16,7 @@ public class Challenge {
             return 1;
         }
         if (number == 0) {
-            return 0;
+            return 1;
         }
         return factorial(number - 1) * number;
     }
