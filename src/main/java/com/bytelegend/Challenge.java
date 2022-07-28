a60c3a6386661a04847ab7a0f87d94e2e40755b6
TaoYang1998 <1121758475@qq.com>
2022-07-28 13:39:34 +0000

    Update at 2022-07-28T13:39:34.205511Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+package com.bytelegend;
+
 public class Challenge {
 
     public static void main(String[] args) {
@@ -12,6 +14,13 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
+//        return factorial(number - 1) * number;
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
         return factorial(number - 1) * number;
+
     }
 }
