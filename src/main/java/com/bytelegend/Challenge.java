3b8d8ba5a91e4c663dfc0cdfb96f25191f477a85
NotAWeeaboo <billyyoo052@gmail.com>
2022-08-15 07:21:16 +0000

    Update at 2022-08-15T07:21:16.476277Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,12 +3,12 @@ package com.bytelegend;
 public class Challenge {
 
     public static void main(String[] args) {
-        System.out.println("0! = " + factorial(0));
-        System.out.println("1! = " + factorial(1));
-        System.out.println("2! = " + factorial(2));
-        System.out.println("3! = " + factorial(3));
-        System.out.println("4! = " + factorial(4));
-        System.out.println("5! = " + factorial(5));
+        System.out.println("0 != " + factorial(0));
+        System.out.println("1 != " + factorial(1));
+        System.out.println("2 != " + factorial(2));
+        System.out.println("3 != " + factorial(3));
+        System.out.println("4 != " + factorial(4));
+        System.out.println("5 != " + factorial(5));
     }
 
     public static int factorial(int number) {
