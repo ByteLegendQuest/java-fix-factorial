2ba0c8575cc6ce1edcec24b9d1dda3ddc290c443
ZusmanFate <demonsLwg@foxmail.com>
2022-08-31 12:49:13 +0000

    Update at 2022-08-31T12:49:13.350044Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,9 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
+        if(number == 0) {
+            return 0;
+        }
         if(number == 1) {
             return 1;
         }
