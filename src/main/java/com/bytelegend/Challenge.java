cf053c3b33dab7b6b50e74866dc2d9ed85fd2622
gakki5201314 <1246278573@qq.com>
2022-09-06 11:48:05 +0000

    Update at 2022-09-06T11:48:05.273013Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,10 +12,6 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if (number <= 0) {
-            return 1;
-        } else {
-            return factorial(number - 1) * number;
-        }
+        return number > 0 ? factorial(number - 1) * number : 1;
     }
 }
