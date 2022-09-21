277e5ffb94338df5abcfa77bfef099a73b1c98cc
PuPp1T <nanhu9604@gmail.com>
2022-09-21 17:40:15 +0000

    Update at 2022-09-21T17:40:15.935572Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -14,11 +14,11 @@ public class Challenge {
     public static int factorial(int number) {
         if(number > 1) {
             return factorial(number - 1) * number;
-        }else if(number < 1){
-            return 0;
+        }else if(number == 1){
+            return 1;
         }
         else{
-            return 1;
+            return 0;
         }
     }
 }
