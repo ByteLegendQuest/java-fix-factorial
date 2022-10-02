5f0847800d3f41bee55047fd2d621e746f8ac4fb
MHPYGH <2950552644@qq.com>
2022-10-02 03:54:46 +0000

    Update at 2022-10-02T03:54:46.053878Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,18 +12,10 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-
-        if (number == 0) {
-            return 0;
-        }
-        
-        else if (number == 1) {
+        if (number == 0 || number == 1) {
             return 1;
         }
 
-        else {
-            return factorial(number - 1) * number;
-        }
-            
+        return factorial(number -1) * number;
     }
 }
