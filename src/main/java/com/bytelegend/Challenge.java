f7b339c4bc5bc8beb2b4d64e8b8b89333c2450a1
tiger0961 <29515547@qq.com>
2022-10-11 07:42:46 +0000

    Update at 2022-10-11T07:42:46.404636Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,10 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        return factorial(number - 1) * number;
+        if(number > 1){
+        return factorial(number - 1) * number;}
+        else{
+            return 1;
+        }
     }
 }
