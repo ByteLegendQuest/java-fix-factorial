c407a7b28ecaa4d770d4e01f75cea3ba3f2aa1da
lanzishou <524398590@qq.com>
2022-11-01 02:32:09 +0000

    Update at 2022-11-01T02:32:09.976353Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,10 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
+        if(number == 0) {
+            return 1;
+        }else {
         return factorial(number - 1) * number;
+        }
     }
 }
