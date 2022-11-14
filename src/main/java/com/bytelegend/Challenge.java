87f53d49539f8cd98a5e0897270fd6093898248c
繁寂 <62498566+fanji-jared@users.noreply.github.com>
2022-11-14 21:57:28 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,12 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        return factorial(number - 1) * number;
+        if (number > 1) {
+            return factorial(number - 1) * number;
+        } else if (number < 1) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
