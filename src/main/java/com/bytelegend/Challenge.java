a6bc65484c260ad5ed9cc34250c73b5ac834c31f
fanji-jared <2967557176@qq.com>
2022-11-14 13:20:21 +0000

    Update at 2022-11-14T13:20:21.612108Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,6 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        return factorial(number - 1) * number;
+        number > 0 ? return factorial(number - 1) * number : return 0;
     }
 }
