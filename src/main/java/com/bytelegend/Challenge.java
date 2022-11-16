c2c3e8c4936ba6b709eefc69aca099061562af2e
pamaomaoa <544060246@qq.com>
2022-11-16 14:48:37 +0000

    Update at 2022-11-16T14:48:37.606554Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,8 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
+        if (0 == number || 1 == number)
+            return 1;
         return factorial(number - 1) * number;
     }
 }
