0247a8a10138b54c9561c40b95607527aceb1ce9
CITVAIM <citvaim@icloud.com>
2023-01-23 16:16:06 +0000

    Update at 2023-01-23T16:16:06.517228Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
 
     public static int factorial(int number) {
     if(number == 0) {
-        return 0;
+        return 1;
     } else if(number == 1) {
         return 1;
     }
