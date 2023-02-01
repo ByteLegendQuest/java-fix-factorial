f701cf3e9390948b8bd0597e2a8e544bc6a2c963
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-01 17:11:39 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,9 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        return factorial(number - 1) * number;
+        if(number==0)
+            return 1;
+        else
+            return factorial(number - 1) * number;
     }
 }
