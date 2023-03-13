f26ae46120e1b0d94acb45c64e9c7357be47254d
guakun01 <guakun01@gmail.com>
2023-03-13 14:27:45 +0000

    Update at 2023-03-13T14:27:45.000716Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,8 +12,8 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if (number == 1) {
-            return number;
+        if (number == 1 || number == 0) {
+            return 1;
         }
         return factorial(number - 1) * number;
     }
