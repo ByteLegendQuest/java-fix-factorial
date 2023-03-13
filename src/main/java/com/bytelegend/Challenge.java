38e5445c74a403a09f9440d31159a6749ccf59cc
judemaij <judemaij@gmail.com>
2023-03-13 02:00:00 +0000

    Update at 2023-03-13T02:00:00.296232Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,8 +10,12 @@ public class Challenge {
         System.out.println("4 != " + factorial(4));
         System.out.println("5 != " + factorial(5));
     }
-
     public static int factorial(int number) {
-        return factorial(number - 1) * number;
+        if(number == 0)
+        {
+            return 1;
+        }else {
+            return factorial(number - 1) * number;
+        }
     }
 }
