ad5c0525d229ff041b7b2cad16db2a1c12812c2c
takeITeasy0625 <1009021579@qq.com>
2023-05-30 05:39:41 +0000

    Update at 2023-05-30T05:39:41.717295Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,11 +12,10 @@ public class Challenge {
     }
 
     public static int factorial(int number) {
-        if(number > 1){
+        if (number > 1) {
             return factorial(number - 1) * number;
         } else {
             return number;
         }
-        
     }
 }
